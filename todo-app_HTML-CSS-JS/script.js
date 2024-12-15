7b6e5865517f2e5f72bbd0ca3fc7d789dde54886
Batajoo <sanjivbatajoo@gmail.com>
2024-12-14 23:46:16 -0600

    sidebar button adjustment done

--- todo-app_HTML-CSS-JS/script.js
+++ todo-app_HTML-CSS-JS/script.js
@@ -1,7 +1,25 @@
 const barIconButton = document.querySelector(".bar-icon")
 const categoriesList = document.querySelector(".categories-list")
 const sideBar = document.querySelector(".sidebar");
-barIconButton.addEventListener("click", (e)=>{
+let sideBarActive = false;
+
+const sideBarAnimation = () => {
+    categoriesList.classList.toggle("categories-list-unactive");
     categoriesList.classList.toggle("categories-list");
+
     sideBar.classList.toggle("sidebar-on");
+    barIconButton.classList.toggle("bar-icon-unactive");
+}
+barIconButton.addEventListener("click", (e)=>{
+    if(!sideBarActive){
+        sideBarAnimation();
+        sideBarActive = true;
+    }
+})
+
+document.addEventListener("click",(e)=>{
+    if(sideBarActive && e.target !== barIconButton && !sideBar.contains(e.target)) {
+        sideBarAnimation();
+        sideBarActive = false;
+    }
 })

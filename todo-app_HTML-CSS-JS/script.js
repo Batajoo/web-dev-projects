c20eee2176210ed0d5eb9c8eae2a07b757b3b503
Batajoo <sanjivbatajoo@gmail.com>
2024-12-15 20:19:54 -0600

    todo add button functionality added

--- todo-app_HTML-CSS-JS/script.js
+++ todo-app_HTML-CSS-JS/script.js
@@ -1,3 +1,23 @@
+const todoInputBox = document.getElementById("todo-text");
+const addTaskButton = document.getElementById("add-task");
+const todoListContainer = document.getElementById("todo-container");
+
+const addTodoToList = ()=>{
+    const todoText = todoInputBox.value;
+    todoListContainer.innerHTML += `
+    <div class="task-box">
+        <label for="task-status"><input type="checkbox" id="task-status"><span class="task-data">${todoText}</span></label>
+        <div class="action-section">
+            <i class="fa-solid fa-pen-to-square task-edit-icons"></i>
+            <i class="fa-solid fa-delete-left task-edit-icons"></i>
+        </div>
+    </div>
+    `;
+};
+addTaskButton.addEventListener("click", addTodoToList);
+
+
+// barIconButton 
 const barIconButton = document.querySelector(".bar-icon")
 const categoriesList = document.querySelector(".categories-list")
 const sideBar = document.querySelector(".sidebar");
